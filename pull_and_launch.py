dbdd26ca9af3d06325f83bd286a124916514cd21
ghostboats_laptop <ghostboats2895@gmail.com>
2024-01-11 08:02:13 -0600

    update

--- pull_and_launch.py
+++ pull_and_launch.py
@@ -4,6 +4,42 @@ import os
 import pyautogui
 import time
 
+def find_image_on_screen(screenshot_path, click = 0, search_time=0, confidence=0.8):
+    print("Starting function find_image_on_screen. Looking for: " + str(screenshot_path))
+    image_location = None
+    moved = False
+    if search_time == 0:
+        while image_location is None:
+            try:
+                image_location = pyautogui.locateOnScreen(screenshot_path, confidence=confidence)
+            except:
+                if moved == True:
+                    print('mouse has been moved off screne already')
+                else:
+                    print('image not found, moving mouse off screen to avoid issues reading screen')
+                    pyautogui.moveTo(pyautogui.size().width + 100, pyautogui.size().height / 2)
+                    moved = True
+                image_location = None
+    else:
+        start_time = time.time()
+        end_time = start_time + search_time
+        while image_location is None and time.time() < end_time:
+            try:
+                image_location = pyautogui.locateOnScreen(screenshot_path, confidence=confidence)
+            except:
+                if moved == True:
+                    print('mouse has been moved off screne already')
+                else:
+                    print('image not found, moving mouse off screen to avoid issues reading screen')
+                    pyautogui.moveTo(pyautogui.size().width + 100, pyautogui.size().height / 2)
+                    moved = True
+                image_location = None
+    if image_location != None and click != 0:
+        print('clicking location')
+        pyautogui.click(image_location)
+    return image_location
+
+
 def run_git_pull():
     try:
         subprocess.check_call(['git', 'pull'])
@@ -12,47 +48,89 @@ def run_git_pull():
         return False
     return True
 
-def upload_mod_file(mod_file, mods_dir):
+def upload_mod_file(mod_folder, mods_dir):
     if not os.path.exists(mods_dir):
         os.makedirs(mods_dir)
-    pack_mod()
-    shutil.copy(mod_file, mods_dir)
+    divine_path = r"C:\Users\vishal\Desktop\bg3_mods\ExportTool-v1.18.7\Tools\divine.exe"
+    pack_mod(os.path.abspath(__file__), divine_path, mod_folder)
+
 
 def pack_mod(folder_path, divine_path, mod_dest_path):
     root_dir = os.path.dirname(folder_path)
-    mod_name = os.path.basename(folder_path)
+    mod_name = os.path.basename(mod_dest_path)  # Assuming mod_dest_path is the mod name
     print('folder_path:' + str(folder_path))
     print('divine_path:' + str(divine_path))
     print('mod_dest_path:' + str(mod_dest_path))
-    quit()
-    pakpath = os.path.join(os.path.dirname(root_dir), "temp", f"{mod_name}.pak")
-    mod_dir = os.path.join(os.path.dirname(root_dir), "bg3_mod_template_tool", mod_name)
+    print(mod_name)
+
+    pakpath = os.path.join(root_dir, "temp", f"{mod_name}.pak")
+    mod_dir = os.path.join(root_dir, mod_dest_path)  # Corrected to point to mod folder
     print('pakpath_dir:' + str(pakpath))
     print('mod_dir:' + str(mod_dir))
-    # Use the provided divine.exe path
-    
-    subprocess.run([divine_path, "-g", "bg3", "--action", "create-package", "--source", mod_dir, "--destination", pakpath, "-l", "all"])
-    if mod_dest_path:
-        shutil.move(pakpath, os.path.join(mod_dest_path, f"{mod_name}.pak"))
-        print("Mod files moved to", mod_dest_path)
-        temp_folder = os.path.join(os.path.dirname(root_dir), "temp")
-        if os.path.exists(temp_folder):
-            shutil.rmtree(temp_folder)
-        return
+
+    result = subprocess.run([divine_path, "-g", "bg3", "--action", "create-package", "--source", mod_dir, "--destination", pakpath, "-l", "all"], capture_output=True, text=True)
+    print(result.stdout)
+    print(result.stderr)
+
+    if os.path.exists(pakpath):
+        shutil.move(pakpath, os.path.join(mods_dir, f"{mod_name}.pak"))
+        print("Mod files moved to", mods_dir)
     else:
-        print("Mod Destination Folder not provided. Please provide it in the Configuration tab.")
-        return
+        print("Error: .pak file not created.")
+    
+    # Clean up temp folder
+    temp_folder = os.path.join(root_dir, "temp")
+    if os.path.exists(temp_folder):
+        shutil.rmtree(temp_folder)
 
 def launch_bg3_mod_manager():
     mod_manager_path = r'C:\Users\vishal\Desktop\bg3_mods\BG3ModManager_Latest\BG3ModManager.exe'
     subprocess.Popen([mod_manager_path])
     time.sleep(5)  # Wait for the mod manager to launch
-    pyautogui.hotkey('ctrl', 'shift', 'g')  # Launch the game
+    inactive_mods_path = r'C:\Users\vishal\Desktop\bg3_mods\inactive_mods.png'
+    quickster_active_path = r'C:\Users\vishal\Desktop\bg3_mods\quickster_active.png'
+    image_path = r'C:\Users\vishal\Desktop\bg3_mods\quickster_location.png'
+
+    find_image_on_screen(inactive_mods_path,1)
+    pyautogui.typewrite('Quickster')
+    quickster_active_location=find_image_on_screen(quickster_active_path)
+    if quickster_active_location == None:
+        find_image_on_screen(image_path)
+    max_retries = 10
+    retry_delay = 2  # seconds
+    for _ in range(max_retries):
+        try:
+            inactive_mods_location = pyautogui.locateCenterOnScreen(inactive_mods_path, confidence=0.9)
+            if inactive_mods_location is not None:
+                pyautogui.click(inactive_mods_location)
+                pyautogui.typewrite('Quickster')
+                quickster_active_location = pyautogui.locateCenterOnScreen(quickster_active_path, confidence=0.9)
+                if quickster_active_location is None:
+                    location = pyautogui.locateCenterOnScreen(image_path, confidence=0.8)
+                    if location is not None:
+                        pyautogui.click(location)
+                        pyautogui.press('enter')
+                        break  # Exit the loop if the image is found and clicked
+                    else:
+                        location = None
+                        print("Retrying to find quickster_location...")
+                else:
+                    break  # Exit the loop if the active location is found
+            else:
+                print("Retrying to find inactive_mods...")
+            time.sleep(retry_delay)
+        except Exception as e:
+            print(f"An error occurred ({type(e).__name__}): {e}")
+            time.sleep(retry_delay)
+
+    pyautogui.hotkey('ctrl', 's')
+    #pyautogui.hotkey('ctrl', 'shift', 'g')  # Launch the game
 
 if __name__ == "__main__":
     if run_git_pull():
         mod_file = 'Quickster.pak'
-        appdata_path = os.getenv('APPDATA')  # This gets the value of the APPDATA environment variable
-        mods_dir = os.path.join(appdata_path, 'Larian Studios\\Baldur\'s Gate 3\\Mods')
-        upload_mod_file(mod_file, mods_dir)
+        mod_folder = 'Quickster'
+        local_appdata_path = os.getenv('LOCALAPPDATA')
+        mods_dir = os.path.join(local_appdata_path, 'Larian Studios\\Baldur\'s Gate 3\\Mods')
+        upload_mod_file(mod_folder, mods_dir)
         launch_bg3_mod_manager()
